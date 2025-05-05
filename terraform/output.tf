4d48348c4a18b4194fbd60aceca1d740f7a4c284
NainaGhosh01 <nainaghosh01@gmail.com>
2025-05-06 00:10:49 +0530

    Update output.tf

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,34 +1,3 @@
-output "eks_cluster_name" {
-  description = "The name of the EKS cluster"
-  value       = aws_eks_cluster.main.name
-}
-
-output "eks_cluster_endpoint" {
-  description = "The endpoint for the EKS cluster"
-  value       = aws_eks_cluster.main.endpoint
-}
-
-output "eks_cluster_ca_certificate" {
-  description = "The certificate authority data for your cluster"
-  value       = aws_eks_cluster.main.certificate_authority[0].data
-}
-
-output "region" {
-  description = "AWS Region"
-  value       = var.region
-}
-
-output "vpc_id" {
-  description = "VPC ID"
-  value       = aws_vpc.main.id
-}
-
-output "public_subnet_ids" {
-  description = "List of public subnet IDs"
-  value       = aws_subnet.public[*].id
-}
-
-output "github_actions_deploy_role_arn" {
-  description = "IAM role ARN for GitHub Actions to assume"
-  value       = aws_iam_role.github_actions_deploy.arn
+output "app_url" {
+  value = "https://${azurerm_app_service.app.default_site_hostname}"
 }
